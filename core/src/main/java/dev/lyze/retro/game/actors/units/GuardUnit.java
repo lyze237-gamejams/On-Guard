78865d4052b47d8a14fa025a555bb08b3aafc2e0
Lyze <11274700+lyze237@users.noreply.github.com>
2020-06-24 18:47:38 +0200

    Added proper sprites for units

--- core/src/main/java/dev/lyze/retro/game/actors/units/GuardUnit.java
+++ core/src/main/java/dev/lyze/retro/game/actors/units/GuardUnit.java
@@ -6,7 +6,7 @@ import dev.lyze.retro.game.actors.units.behaviours.MovementBehaviour;
 
 public class GuardUnit extends Unit {
     public GuardUnit(Game game, boolean playerUnit) {
-        super(game, game.getAss().getSkullUnit(), playerUnit, 8);
+        super(game, game.getAss().getGuardUnit(), playerUnit, 8);
 
         addBehaviour(new AttackBehaviour(this, 4));
         addBehaviour(new MovementBehaviour(this, 2));
